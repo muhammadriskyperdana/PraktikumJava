e7fb61d836ab9aaf10bccf0bfe6151877b9a8107
muhammadriskyperdana <ikibos13@gmail.com>
2021-05-24 16:15:39 +0800

    deklarasi variable dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[]args){
+  String namaDepan = "Muhammad";
+  String namaTengah = "Risky";
+  String namaBelakang = "Perdana";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.99764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
